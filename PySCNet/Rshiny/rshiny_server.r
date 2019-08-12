26376179a731791dd0327228436052ebec5444f1
MingBit <angela.wu1101@gmail.com>
2019-08-12 16:54:46 +0200

    R shiny

--- PySCNet/Rshiny/rshiny_server.r
+++ PySCNet/Rshiny/rshiny_server.r
@@ -1,8 +1,10 @@
 library(ggpubr)
 library(dplyr)
+library(igraph)
 
 server = function(input, output) {
   source_python('~/MING_V9T/PhD_Pro/PySCNet/PySCNet/Preprocessing/gnetdata.py')
+  
   pk_upload <- reactive({
     inFile <- input$file1
     if (is.null(inFile))
@@ -21,6 +23,7 @@ server = function(input, output) {
   })
   
   output$exp <- DT::renderDataTable({
+    
     pickle_data <- pk_upload()
     datatable(pickle_data$ExpMatrix)%>% 
       formatStyle(names(pickle_data$ExpMatrix), backgroundColor = "grey") %>% 
@@ -35,28 +38,6 @@ server = function(input, output) {
     pickle_data <- pk_upload()
     datatable(pickle_data$GeneAttrs) %>% formatStyle(names(pickle_data$GeneAttrs), backgroundColor = "grey") %>%
       formatStyle(0, target = 'row', backgroundColor = 'black', fontWeight = 'bold')})
-  
-  output$links <- DT::renderDataTable({
-    # pickle_data <- pk_upload()
-    # datatable(data.frame(source = pickle_data$NetAttrs$links['source'],
-    #                      target = pickle_data$NetAttrs$links['target'],
-    #                      weight = pickle_data$NetAttrs$links['weight'])) %>% 
-    links <- get_links() 
-    datatable(links, options = list(lengthMenu = c(5, 30, 50), pageLength = 5)) %>%
-      formatStyle(c('cell_clusterid', 'source', 'target', 'weight'), backgroundColor = "grey") %>%
-      formatStyle(0, target = 'row', backgroundColor = 'black', fontWeight = 'bold')
-  })
-  
-  output$gene_module <- DT::renderDataTable({
-    # pickle_data <- pk_upload()
-    links <- get_links()
-    # datatable(data.frame(source = pickle_data$NetAttrs$links['source'],
-    #                      target = pickle_data$NetAttrs$links['target'],
-    #                      weight = pickle_data$NetAttrs$links['weight'])) %>% 
-    datatable(links, options = list(lengthMenu = c(5, 30, 50), pageLength = 5)) %>%
-      formatStyle(c('cell_clusterid','source', 'target', 'weight'), backgroundColor = "grey") %>%
-      formatStyle(0, target = 'row', backgroundColor = 'black', fontWeight = 'bold')
-  })
   
   output$cell_summary <- renderPlot({
     pickle_data <- pk_upload()
@@ -79,16 +60,56 @@ server = function(input, output) {
     hist(pickle_data$NetAttrs$links['weight'])
   })
   
-  output$network <- renderForceNetwork({
-    # links <- get_links()
-    # gene_info <- pk_upload()$GeneAttrs
-    data("MisLinks")
-    data("MisNodes")
-    forceNetwork(Links = MisLinks, Source = "source", Nodes = MisNodes,
-                 Target = "target", Value = "value", NodeID = "name",
-                 height = 500, width = 1000, fontSize = input$label_size, zoom = TRUE,
-                 Group = "group", opacity = input$node_opacity,  colourScale = JS("d3.scaleOrdinal(d3.schemeCategory20);"))
+  output$clusterid <- renderUI({
+    
+    links <- get_links()
+    selectInput('cluster_id', 'Select Cluster ID', choices = as.list(unique(links$cell_clusterid)), selected = '1')
+    
   })
+
+  output$genemodule <- renderUI({
+    
+    links <- get_links()
+    selectInput('gene_module', 'Select Gene Module', choices = as.list(unique(links$cell_clusterid)), selected = '1')
+    
+  })
+  
+  output$links <- DT::renderDataTable({
+    
+    links <- get_links() 
+    datatable(links, options = list(lengthMenu = c(5, 30, 50), pageLength = 10), filter = 'top', style = 'jqueryui') %>%
+      formatStyle(c('cell_clusterid', 'source', 'target', 'weight'), backgroundColor = "grey") %>%
+      formatStyle(0, target = 'row', backgroundColor = 'black', fontWeight = 'bold')
+  })
+  
+  output$gene_module <- DT::renderDataTable({
+    
+    links <- get_links()
+    datatable(links, options = list(lengthMenu = c(5, 30, 50), pageLength = 10), style = 'jqueryui') %>%
+      formatStyle(c('cell_clusterid','source', 'target', 'weight'), backgroundColor = "grey") %>%
+      formatStyle(0, target = 'row', backgroundColor = 'black', fontWeight = 'bold')
+  })
+  
+  output$network <- renderForceNetwork({
+    
+    pickle_data <- pk_upload()
+    gene_info <- pickle_data$GeneAttrs %>% mutate(id = seq(0, nrow(.)-1))
+    
+    links <- get_links() %>% dplyr::filter(cell_clusterid == input$cluster_id) %>% 
+      mutate(source_id = gene_info[match(source, gene_info$Gene),'id']) %>%
+      mutate(target_id = gene_info[match(target, gene_info$Gene),'id'])
+    
+    links <- links[order(links$weight, decreasing = T), ][1:as.numeric(input$top_edges),]
+    
+    
+    forceNetwork(Links = links,
+                 Source = "source_id", Nodes = gene_info, opacityNoHover = 1,linkWidth = 5,
+                 Target = "target_id", Value = 1, NodeID = "Gene", Group = 'TF_Gene',
+                 height = 500, width = 1000, fontSize = input$label_size, zoom = TRUE,
+                 opacity = input$node_opacity, colourScale = JS("d3.scaleOrdinal(d3.schemeCategory20);"))
+  })
+  
+
   
   observeEvent(input$clean_button, {
     shinyjs::reset("sidebar")
