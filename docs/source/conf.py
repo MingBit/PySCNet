e8a05a5bdb49c5243e48d6e7bcb30aa6bf5b2eba
MingBit <angela.wu1101@gmail.com>
2020-07-30 14:21:14 +0200

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../pyscnet'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information -----------------------------------------------------
 
