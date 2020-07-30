e990f83447e7fcff04f8e2a4c5778381b7142d64
MingBit <angela.wu1101@gmail.com>
2020-07-30 12:28:33 +0200

    clean

--- setup.py
+++ setup.py
@@ -22,9 +22,6 @@ setuptools.setup(
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
